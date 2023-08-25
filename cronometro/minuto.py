816f68976fce749fc297b7fea0a264d397e00af6
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2023-08-24 19:57:46 -0500

    Eliminacion subcarpeta cronometro

--- cronometro/minuto.py
+++
@@ -1,6 +0,0 @@
-from unidadtiempo import UnidadTiempo
-
-class Minuto(UnidadTiempo):
-    def __init__(self):
-        self.valor=0
-        self.tope=60
---
+++ hora.py
@@ -0,0 +1,6 @@
+from unidadtiempo import UnidadTiempo
+
+class Hora(UnidadTiempo):
+    def __init__(self):
+        self.valor=0
+        self.tope=60
