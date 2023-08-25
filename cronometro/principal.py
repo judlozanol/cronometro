1d5ca96c89e959c7ebbdc53d1ebb0c0e4addcc03
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2023-08-24 19:52:23 -0500

    Formato de cronometro

--- cronometro/principal.py
+++ cronometro/principal.py
@@ -1,6 +1,27 @@
 from cronometro import Cronometro
 
 c= Cronometro()
-for i in range(4000):
-    print(c.hora.valor, c.minuto.valor, c.segundo.valor)
+for i in range(5000):
+    if c.segundo.valor//10==0:
+        if c.minuto.valor//10==0:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":0"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":0"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+        else:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+    else:
+        if c.minuto.valor//10==0:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":0"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":0"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+        else:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
     c.avanzar()
